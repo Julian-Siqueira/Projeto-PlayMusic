9fd55e6268336538d9d06f6b6292bb73bdf90802
Julian-Siqueira <juliansiqueiradev@gmail.com>
2024-10-02 10:57:27 -0300

    Criação da lógica do aplicativo.

--- app/src/main/java/com/example/playmusic/MainActivity.java
+++ app/src/main/java/com/example/playmusic/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.playmusic;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,10 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView play,stop,pause;
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,25 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.comopodetudomudar);
+
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
+        stop = findViewById(R.id.stop);
+
+        play.setOnClickListener(view -> {
+            mediaPlayer.start();
+        });
+
+        pause.setOnClickListener(view -> {
+            mediaPlayer.pause();
+        });
+
+        stop.setOnClickListener(view -> {
+            mediaPlayer.stop();
+            mediaPlayer = MediaPlayer.create(this, R.raw.comopodetudomudar);
+        });
+
     }
 }
