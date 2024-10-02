01bd97919dbd01c4f81abcdb334d0e4d4cdbd74d
Julian-Siqueira <juliansiqueiradev@gmail.com>
2024-10-02 12:12:19 -0300

    Implementado ciclo de vida da activity,e tratamento de erro.

--- app/src/main/java/com/example/playmusic/MainActivity.java
+++ app/src/main/java/com/example/playmusic/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.playmusic;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,7 +14,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView play,stop,pause;
+    private ImageView play,stop,pause,imgAlbum;
     private MediaPlayer mediaPlayer;
 
 
@@ -27,24 +29,84 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.comopodetudomudar);
+        //mediaPlayer = MediaPlayer.create(this, R.raw.comopodetudomudar);
 
         play = findViewById(R.id.play);
         pause = findViewById(R.id.pause);
         stop = findViewById(R.id.stop);
+        imgAlbum = findViewById(R.id.album);
 
         play.setOnClickListener(view -> {
-            mediaPlayer.start();
+            if (mediaPlayer != null){
+                mediaPlayer.start();
+            }
         });
 
         pause.setOnClickListener(view -> {
-            mediaPlayer.pause();
+            if (mediaPlayer.isPlaying()){
+                mediaPlayer.pause();
+            }
         });
 
         stop.setOnClickListener(view -> {
-            mediaPlayer.stop();
-            mediaPlayer = MediaPlayer.create(this, R.raw.comopodetudomudar);
+            if (mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+                mediaPlayer = MediaPlayer.create(this, R.raw.comopodetudomudar);
+            }
         });
 
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        if (mediaPlayer != null){
+            mediaPlayer.start();
+        }else {
+            imgAlbum.setImageResource(R.drawable.erromusic);
+            Toast.makeText(this, "Sem Música Encontrada", Toast.LENGTH_LONG).show();
+        }
+        Log.i("LIFE CYCLE", "START");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+
+        Log.i("LIFE CYCLE", "RESUME");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+
+        Log.i("LIFE CYCLE", "PAUSE");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        if (mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+        }
+        Log.i("LIFE CYCLE", "STOP");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+
+        Log.i("LIFE CYCLE", "RESTART");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null && mediaPlayer.isPlaying()){
+            mediaPlayer.stop();
+            mediaPlayer.release();
+            mediaPlayer = null;
+            Log.i("LIFE CYCLE", "DESTROY");
+        }
     }
 }
